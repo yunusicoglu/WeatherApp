56bafdf2db03bf60e814ccbb81a9d096cb75db1a
Yunus İçoğlu <yunusicoglu@gmail.com>
2024-10-04 23:42:42 +0300

    responsive tasarım eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import './App.css'
 import { useState } from 'react'
 import axios from 'axios'
@@ -6,25 +6,28 @@ import { CiSearch } from 'react-icons/ci'
 
 function App() {
 const [cityInput, setCityInput] = useState('')
-// const [city, setCity] = useState('')
+const [city, setCity] = useState('istanbul')
 const [weatherData, setWeatherData] = useState(null)
 const [formattedWeatherData, setFormattedWeatherData] = useState(null)
 
   useEffect(() => {
+    setCity(cityInput)
+  }, [cityInput])
+
+  useEffect(() => {
     const fetchData = async () => {
-      const city = cityInput ? cityInput : 'istanbul'
       try {
         const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=630b0db8e95e45998a7172611242409&q=${city}&days=9&aqi=yes&alerts=yes`)
         setWeatherData(response.data)
-        console.log(response.data);
-        console.log(import.meta.env.VITE_WEATHER_API);
+        console.log('response.data: ',response.data);
 
       } catch (error) {
         console.log(error);
       }
     }
     fetchData()
-  }, [cityInput])
+  }, [city])
+  
 
   useEffect(() => { //veride değişiklikler yapar
     const formattedDay = weatherData?.forecast.forecastday.map((day,i)=>{
@@ -66,83 +69,71 @@ const [formattedWeatherData, setFormattedWeatherData] = useState(null)
         <div className=''>
         </div>
         <div className='top-section'>
-          <div className='city-name'>{formattedWeatherData?.location.region}</div>
-          <div className='div-input-today'>
-            <div className='div-input'>
-              <input 
-                className='cityInput' 
-                type="text"
-                value={cityInput} 
-                onChange={handleInputChange}
-                placeholder='Bir şehir giriniz...'
-              />
-              <div className='div-icon'><CiSearch style={{color:"white", fontSize:"30px", marginRight:"10px"}}/></div>
-            </div>
-            <div className='today-card'>
-              {formattedWeatherData && (
-                <div>
-                  <div style={{display:"flex", justifyContent:"center", fontSize:"22px"}}>
-                    {formattedWeatherData?.forecast.forecastday[0].dayname}
-                  </div>
-                  <div style={{display:"flex", justifyContent:"center", fontSize:"13px", marginTop:"5px"}}>
-                    ({formattedWeatherData?.forecast.forecastday[0].date})
-                  </div>
-                  <div style={{display:"flex", justifyContent:"center"}}>
-                    <img 
-                      src={formattedWeatherData?.forecast.forecastday[0].day.condition.icon} 
-                      alt={formattedWeatherData?.forecast.forecastday[0].day.condition.text} 
-                    />
-                  </div>
-                  <div style={{display:"flex", justifyContent:"center", fontSize:"20px"}}>
-                    {Math.floor(formattedWeatherData?.forecast.forecastday[0].day.avgtemp_c)}°
-                  </div>
+          <div className='city-name' key={formattedWeatherData?.location.region}>{formattedWeatherData?.location.region}</div>
+          <div className='div-input'>
+            <input 
+              className='city-input' 
+              type="text"
+              value={cityInput} 
+              onChange={handleInputChange}
+              placeholder='Bir şehir giriniz...'
+            />
+            <div className='div-icon'><CiSearch style={{color:"white", fontSize:"30px", marginRight:"10px"}}/></div>
+          </div>
+          <div className='today-card'>
+            {formattedWeatherData && (
+              <div>
+                <div style={{display:"flex", justifyContent:"center", fontSize:"22px"}}>
+                  {formattedWeatherData?.forecast.forecastday[0].dayname}
                 </div>
-              )}
-            </div>
+                <div style={{display:"flex", justifyContent:"center", fontSize:"13px", marginTop:"5px"}}>
+                  ({formattedWeatherData?.forecast.forecastday[0].date})
+                </div>
+                <div style={{display:"flex", justifyContent:"center"}}>
+                  <img 
+                    src={formattedWeatherData?.forecast.forecastday[0].day.condition.icon} 
+                    alt={formattedWeatherData?.forecast.forecastday[0].day.condition.text} 
+                  />
+                </div>
+                <div style={{display:"flex", justifyContent:"center", fontSize:"20px"}}>
+                  {Math.floor(formattedWeatherData?.forecast.forecastday[0].day.avgtemp_c)}°
+                </div>
+              </div>
+            )}
           </div>
-          </div>
-        <div className='div-other-days'>
-          { formattedWeatherData
-            ? (
-              formattedWeatherData.forecast.forecastday.map((day,i) => (
-                i === 0 
-                ? null 
-                : (
-                  <div className='other-days' key={day.date}>
-                    <div style={{display:"flex", justifyContent:"center", fontSize:"22px"}}>
-                      {day.dayname}
-                    </div>
-                    <div style={{display:"flex", justifyContent:"center", fontSize:"13px", marginTop:"5px"}}>
-                      ({day.date})
-                    </div>
-                    <div style={{display:"flex", justifyContent:"center"}}>
-                      <img 
-                        src={day.day.condition.icon} 
-                        alt={day.day.condition.text} 
-                      />
-                    </div>
-                    <div style={{display:"flex", justifyContent:"center", fontSize:"20px"}}>
-                      {Math.floor(day.day.avgtemp_c)}°
-                    </div>
-                  </div>
-                )
-              )) 
-            )
-            : (
-              <div style={{display:"flex", width:"100%", justifyContent:"space-evenly"}}>
-                <div className='other-days'></div>
-                <div className='other-days'></div>
-                <div className='other-days'></div>
-                <div className='other-days'></div>
-                <div className='other-days'></div>
-                <div className='other-days'></div>
-                <div className='other-days'></div>
-                <div className='other-days'></div>
+        </div>
+          { formattedWeatherData &&
+            (
+              <div className='div-all-other-days'>
+
+                {formattedWeatherData.forecast.forecastday.map((day,i) => (
+                  i === 0 
+                  ? null 
+                  : (
+                      <div className='div-other-days' key={day.date}>
+                        <div className='other-days'>
+                          <div style={{display:"flex", justifyContent:"center", fontSize:"22px"}}>
+                            {day.dayname}
+                          </div>
+                          <div style={{display:"flex", justifyContent:"center", fontSize:"13px", marginTop:"5px"}}>
+                            ({day.date})
+                          </div>
+                          <div style={{display:"flex", justifyContent:"center"}}>
+                            <img 
+                              src={day.day.condition.icon} 
+                              alt={day.day.condition.text} 
+                            />
+                          </div>
+                          <div style={{display:"flex", justifyContent:"center", fontSize:"20px"}}>
+                            {Math.floor(day.day.avgtemp_c)}°
+                          </div>
+                        </div>
+                      </div>
+                  )
+                ))} 
               </div>
             )
           }
-
-        </div>
       </div>
     </>
   )
